284e973c87bf2b85f33527998722bfad597ead4d
youshiyu <444794227@qq.com>
2020-07-03 03:34:34 +0800

    老人监护系统

--- src/views/friends.vue
+++ src/views/friends.vue
@@ -1,58 +1,74 @@
 <template>
-
   <div class="friend">
     <div class="information">
-      <lg-header :leftArrow="false"
-                 :titleType="1"
-                 style="box-shadow:0px 12px 8px -12px hsl(315, 51%, 85%); border-radius:5px;"
-                 title="相关信息">
-      </lg-header>
+      <lg-header
+        :leftArrow="false"
+        :titleType="1"
+        style="box-shadow:0px 12px 8px -12px hsl(315, 51%, 85%); border-radius:5px;"
+        title="相关信息"
+      ></lg-header>
       <div></div>
-      <input class="list"
-             type="button"
-             value="紧急联系人 " />
-      <input class="loginButton"
-             type="button"
-             value="联系人关系 :" />
-      <span class="call">儿子 </span>
-      <input class="loginButton"
-             type="button"
-             value="联系人电话 :" />
-      <span class="call">17816110557 </span>
-      <div style="background:linear-gradient(to left,#efefef,#b6b6b6,#efefef);height:1px; margin:5px"></div>
-      <input class="list"
-             type="button"
-             value="健康信息 " />
-      <input class="loginButton"
-             type="button"
-             value="每分钟心跳 :" />
-      <span class="call">70次/分钟 </span>
-      <input class="loginButton"
-             type="button"
-             value="健康状态 :" />
-      <span class="call">正常 </span>
-      <div style="background:linear-gradient(to left,#efefef,#b6b6b6,#efefef);height:1px; margin:5px"></div>
-      <input class="list"
-             type="button"
-             value="环境状态 " />
-      <input class="loginButton"
-             type="button"
-             value="温度:" />
-      <span class="call">30 °C </span>
-      <input class="loginButton"
-             type="button"
-             value="湿度 :" />
-      <span class="call">60% RH </span>
+      <input class="list" type="button" value="紧急联系人 " />
+      <input class="loginButton" type="button" value="联系人关系 :" />
+      <span class="call">儿子</span>
+      <input class="loginButton" type="button" value="联系人电话 :" />
+      <span class="call">17816110557</span>
+      <!-- //这个是点击这个链接调用客户端发，需要手动 -->
+      <!-- <a href="mailto:444794227@qq.com">这个好像必须要点击这个链接</a> -->
+      <div
+        style="background:linear-gradient(to left,#efefef,#b6b6b6,#efefef);height:1px; margin:5px"
+      ></div>
+      <input class="list" type="button" value="健康信息 " />
+      <input class="loginButton" type="button" value="每分钟心跳 :" />
+      <span class="call">{{parseInt (this.rate)}}次/分钟</span>
+      <input class="loginButton" type="button" value="健康状态 :" />
+      <span class="call">正常</span>
+      <div
+        style="background:linear-gradient(to left,#efefef,#b6b6b6,#efefef);height:1px; margin:5px"
+      ></div>
+      <input class="list" type="button" value="环境状态 " />
+      <input class="loginButton" type="button" value="温度:" />
+      <span class="call">28 °C</span>
+      <input class="loginButton" type="button" value="湿度 :" />
+      <span class="call">60% RH</span>
     </div>
   </div>
-
 </template>
 
 <script>
 import { Divider } from 'vant';
 export default {
   data () {
-    return {}
+    return {
+       rate:60
+    }
+  },
+  mounted(){
+    var _this=this;
+
+   function getHeartrate()
+   {
+         var x = Math.random();
+         var y;
+         var x = Math.random();
+         var y;
+         if(x < 0.05)
+	       y = 50 + Math.random()*10;
+         else if(x > 0.95)
+	       y = 100 + Math.random()*10;
+         else
+         y = 60 + Math.random()*40;
+
+         _this.rate=y;
+          if(y<55)
+         alert("老人心率过低，请注意")
+         if(y>100)
+         alert("老人心率过高，请注意")
+
+
+   }
+    getHeartrate();
+    setInterval(getHeartrate, 1000 * 10); //每隔30s执行一次
   },
   methods: {
 
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,102 +1,169 @@
 <template>
   <div>
-    <lg-header :leftArrow="false"
-               :titleType="1"
-               title="路线记录"></lg-header>
+    <lg-header :leftArrow="false" :titleType="1" title="路线记录"></lg-header>
     <div class="list">
-      <div id="container">
-
-      </div>
+      <div id="container"></div>
     </div>
   </div>
 </template>
 
 <script>
-import Map from "./map"
-import axios from '@/utils/axios';
-
+import Map from "./map";
+import axios from "@/utils/axios";
+import storage from "good-storage";
 export default {
-  name: 'Home',
-  data () {
+  name: "Home",
+  data() {
     return {
-      message: '',
-    }
+      jindu: "", //注意这里是字符串类型，在自定义的gps类中要转化成数字
+      weidu: "",
+      message: "xxx",
+      totalMarker: [],
+      alarm:false //报警选项,后来没用
+    };
   },
-  mounted () {
-    //先试一下游标获取,测试只能通过OBS获取了，gg
-    // axios.get("")
-    //   .then(response => {
-    //     console.log(response);
-    //     // this.message = response.partition_cursor;
-    //     // console.log(this.message);
-    //   });
-    var marker, map = new AMap.Map("container", {
+  mounted() {
+    var map = new AMap.Map("container", {
       resizeEnable: true,
       center: [120.03933, 30.22829],
-      zoom: 15,
+      zoom: 16
+    });
+    var _this = this;
+    function getready() {
+      axios
+        .get("http://obs8633.obs.cn-north-4.myhuaweicloud.com")
+        .then(response => {
+          const len = response.data.length;
+          //  console.log(response.data.slice(len-235,len-180));
+          const str = response.data.slice(len - 235, len - 180);
+          let key = [];
+          for (let i = 0; i < str.length; i++) {
+            if (
+              str[i] == "<" &&
+              str[i + 1] == "K" &&
+              str[i + 2] == "e" &&
+              str[i + 3] == "y"
+            ) {
+              i = i + 5;
+              while (1) {
+                key.push(str[i]);
+                i++;
+                if (
+                  str[i] == "<" &&
+                  str[i + 1] == "/" &&
+                  str[i + 2] == "K" &&
+                  str[i + 3] == "e"
+                )
+                  break;
+              }
+            }
+          }
+          console.log(_this.message); //这个是访问不到的，因为mounted函数中的this指向window
+          // console.log(key.join(""));
+          //  console.log('`````````'+this.name);
+          let url = `https://obs8633.obs.cn-north-4.myhuaweicloud.com/${key.join("")}`;
+          //先试一下游标获取,测试只能通过OBS获取了，gg
+          console.log(url);
+
+          axios.get(`${url}`).then(response => {
+            const indexOfFirst=response.data.indexOf('alarm') 
+            console.log(response.data.slice(indexOfFirst+8,indexOfFirst+9)) //0|1
+            let isalarm=response.data.slice(indexOfFirst+8,indexOfFirst+9);
+            if(Number(isalarm)==1){
+              alert("老人报警了");
+            }
+
+            //if( response.data.slice(80, 90))
+            console.log(typeof response.data);
+            if (typeof response.data == "object") {
+              console.log(response.data.services[0].properties.longitude);
+              console.log(response.data.services[0].properties.latitude);
+              _this.jindu = response.data.services[0].properties.longitude;
+              _this.weidu = response.data.services[0].properties.latitude;
+            } else {
+              console.log("经度:", response.data.slice(80, 90)); //120.036263
+              console.log("纬度:", response.data.slice(104, 113)); // 30.230240
+              //经纬度
+              _this.jindu = response.data.slice(80, 90);
+              _this.weidu = response.data.slice(104, 113);
+              //es6构造函数
+            }
+
+            //每30获取一次，应该会不一样的,这是之前str的时候获取方式，今天突然变成json了
+            class Gps {
+              constructor(jin, wei) {
+                this.jin = jin;
+                this.wei = wei;
+              }
+            } //Gps是个对象类
+            const gps = new Gps(_this.jindu, _this.weidu);
+            _this.totalMarker.push(gps);
+            console.log(_this.totalMarker, "~~~~~~~", _this.totalMarker.length);
+          });
+        });
+
+      // 坐标转换,并且加点
+      function convertFrom(lnglat, type) {
+        AMap.convertFrom(lnglat, type, function(status, result) {
+          if (result.info === "ok") {
+            var m2;
+            var resLnglat = result.locations[0];
+            m2 = new AMap.Marker({
+              position: resLnglat
+            });
+            //他把添加坐标的也搞到坐标转换里了，可以最后统一加
+            map.add(m2); //这个就每次加一个点,这是加蓝色的坐标位置
+          }
+        });
+      }
+      //
+      function convertFromred(lnglat, type) {
+        AMap.convertFrom(lnglat, type, function(status, result) {
+          if (result.info === "ok") {
+            var resLnglat = result.locations[0];
+            var m1 = new AMap.Marker({
+              position: resLnglat,
+              icon: "https://webapi.amap.com/theme/v1.3/markers/n/mark_r.png"
+            });
+            //他把添加坐标的也搞到坐标转换里了，可以最后统一加
+            map.add(m1); //这个就每次加一个点,这是加蓝色的坐标位置
+          }
+        });
+      }
+      for (let j = 0; j < _this.totalMarker.length; j++) {
+        //  var lnglat = [120.036263,30.230240]; //这个是gps的经纬度
+        var lnglat = [
+          Number(_this.totalMarker[j].jin),
+          Number(_this.totalMarker[j].wei)
+        ];
+        // console.log('奇怪这一句',j);
+        if (j == _this.totalMarker.length - 1) //终点的画红色
+        convertFromred(lnglat, "gps");
+        else
+         convertFrom(lnglat, "gps");
+      }
     }
-    );
-    var marker1 = new AMap.Marker({
-      icon: "https://webapi.amap.com/theme/v1.3/markers/n/mark_b.png",
-      position: [120.03943, 30.22829],
-      offset: new AMap.Pixel(-13, -30)
-    });
-    var marker2 = new AMap.Marker({
-      icon: "https://webapi.amap.com/theme/v1.3/markers/n/mark_r.png",
-      position: [120.03933, 30.22847],
-      offset: new AMap.Pixel(-13, -30)
-    });
-    map.add(marker1);
-    map.add(marker2);
-    map.setFitView();
-  }
-}
 
+    getready();
 
-
-
-function updateContent () {
-
-  if (!marker) {
-    return;
+    setInterval(getready, 1000 * 30); //每隔30s执行一次
   }
 
-  // 自定义点标记内容
-  var markerContent = document.createElement("div");
-
-  // 点标记中的图标
-  var markerImg = document.createElement("img");
-  markerImg.className = "markerlnglat";
-  markerImg.src = "下载.png";
-  markerContent.appendChild(markerImg);
-
-  // 点标记中的文本
-  var markerSpan = document.createElement("span");
-  markerSpan.className = 'marker';
-  markerSpan.innerHTML = "Hi，我被更新啦！";
-  markerContent.appendChild(markerSpan);
-
-  marker.setContent(markerContent); //更新点标记内容
-  marker.setPosition([120.419, 30.3]); //更新点标记位置
-}
-
-// 清除 marker
-function clearMarker () {
-
-  if (marker) {
-    marker.setMap(null);
-    marker = null;
-  }
-}
-
+  // beforeDestroy() {
+  //   //销毁定时器
+  //   clearInterval(this.timer);
+  // }
+};
 </script>
+
+
 <style lang="scss" scoped>
 .list {
   height: 100%;
   padding-top: 46px;
 }
 #container {
-  height: 95%;
+  height: 98%;
   width: 100%;
   overflow: hidden;
   z-index: -100;
